c3c03aa63f6cb03d41b6885b34d443c70ef8a43c
pesselotis <pessel@jimmyjohns.com>
2020-03-29 23:04:34 -0500

    Removed unused variable.

--- src/com/company/arraysbasics/SumArray.java
+++ src/com/company/arraysbasics/SumArray.java
@@ -4,7 +4,7 @@ public class SumArray {
     public static void main(String[] args) {
         String[] numbers = {"5", "10", "12", "20", "23", "30"};
         int sum = 0;
-        int i = 0;
+
         for (String tempNum : numbers) {
             sum = sum + Integer.parseInt(tempNum);
         }
